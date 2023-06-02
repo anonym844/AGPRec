c0a87190b39c53d102eff2bbb5c5024beb1642a5
anonym844 <104882881+anonym844@users.noreply.github.com>
2023-06-02 11:00:46 +0800

    Update and rename AGPRec.py to IGRec.py

--- models/IGRec.py
+++ models/IGRec.py
@@ -26,7 +26,7 @@ class LightGCNLayer(nn.Module):
             src, _ , dst = etype
             feat_src = h[src]
             feat_dst = h[dst]
-            aggregate_fn = fn.copy_src('h', 'm')
+            aggregate_fn = fn.copy_u('h', 'm')
 
             degs = graph.out_degrees(etype = etype).float().clamp(min = 1)
             norm = torch.pow(degs, -0.5)
@@ -57,9 +57,9 @@ class self_gate(nn.Module):
         gate = torch.sigmoid(gate)
         return embedding * gate
     
-class AGPRec(nn.Module):
+class IGRec(nn.Module):
     def __init__(self,args,graph_g_u,graph,device):
-        super(AGPRec, self).__init__()
+        super(IGRec, self).__init__()
         #parameter 
         self.embedding_dim = args.embed_size
         self.num_users = graph.nodes('user').shape[0]
@@ -70,10 +70,17 @@ class AGPRec(nn.Module):
         self.item_embedding = torch.nn.Parameter(torch.randn(graph.nodes('item').shape[0], self.embedding_dim))
         self.attention = torch.nn.Parameter(torch.randn(1, self.embedding_dim))
         self.groupembeds = torch.nn.Parameter(torch.randn(self.num_groups, self.embedding_dim))
+        # self.aspects = torch.nn.Parameter(torch.randn(self.num_users, self.num_aspects, self.embedding_dim))
+        
         self.aspects = torch.nn.ModuleList()
         for i in range(args.num_aspects):
             self.aspects.append(self_gate(self.embedding_dim))
         self.softmax = nn.Softmax(dim = 0)
+        # self.attention = AttentionLayer(2 * self.embedding_dim, args.drop_ratio)
+        # self.predictlayer = PredictLayer(3 * embedding_dim, drop_ratio)
+
+
+        # self.num_heads = args.num_heads
         self.graph_g_u = dgl.edge_type_subgraph(graph_g_u,[('group', 'contain', 'user')])
         self.graph_u_g = dgl.edge_type_subgraph(graph_g_u,[('user', 'in', 'group')])
         self.device = device
@@ -93,7 +100,6 @@ class AGPRec(nn.Module):
         self.threshold = args.threshold
         self.reg_coef = args.reg_coef
 
-
     def build_layer(self, idx):
         return LightGCNLayer()
 
@@ -105,7 +111,8 @@ class AGPRec(nn.Module):
 
 
     def forward(self, graph, group_user):
-        user_embed = self.g_u_forward(group_user)
+        user_embed, group_emb = self.g_u_forward(group_user)
+        user_embed = [user_embed]
         item_embed = [self.item_embedding]
         h = self.node_features
 
@@ -121,15 +128,15 @@ class AGPRec(nn.Module):
         item_embed = torch.mean(torch.stack(item_embed, dim = 0), dim = 0)
 
         h = {'user': user_embed, 'item': item_embed}
-        return user_embed, item_embed
+        return user_embed, item_embed, group_emb
 
     def aspect_regular(self):
         #aspect regularization
         if self.isReg:
             div_metric = None
             # N x K x dim
-            for i in range(len(self.aspects)):
-                for j in range(i + 1, len(self.aspects)):
+            for i in range(self.num_aspects):
+                for j in range(i + 1, self.num_aspects):
                     sim_matrix = F.cosine_similarity(self.user_embed_asp[i], self.user_embed_asp[j])
                     mask = torch.abs(sim_matrix) > self.threshold
                     if i == 0 and j == 1:
@@ -152,7 +159,7 @@ class AGPRec(nn.Module):
         embed_centers = self.groupembeds
         #Apply softmax
         #new
-        embed_member_agg = torch.zeros(self.num_groups, len(self.aspects), self.embedding_dim).to(group_user.device)
+        embed_member_agg = torch.zeros(self.num_groups, self.num_aspects, self.embedding_dim).to(group_user.device)
 
         
         for j in range(self.num_groups):
@@ -161,19 +168,32 @@ class AGPRec(nn.Module):
             H = asp * self.attention.unsqueeze(0)
             H_sum = H.sum(-1)
             weight = torch.softmax(H_sum,dim=1)
+            # pdb.set_trace()
             aggregation = (weight.unsqueeze(-1) * asp).sum(1)
             embed_member_agg[j] = aggregation
-    
+        
+        # embed_member_means = torch.stack([torch.mm(group_user,user_embed_asp[i])/group_user.sum(1).unsqueeze(1) for i in range(len(self.aspects))],dim=1)
+        # learned_group_embed = embed_member_means.mean(1)
+        
+        # aspect_softmax = torch.softmax(torch.bmm(embed_member_agg, embed_centers.unsqueeze(-1)),dim=1).squeeze(-1)
+
+
         aspect_softmax = torch.bmm(embed_member_agg, embed_centers.unsqueeze(-1)).squeeze(-1)
         aspect_softmax = F.gumbel_softmax(aspect_softmax, tau=self.tau_gumbel, hard=self.isHard)
         learned_group_embed = (embed_member_agg*aspect_softmax.unsqueeze(-1)).sum(1)
 
+        # #remove aspect
+        # learned_group_embed = torch.mm(group_user,self.user_embedding)/group_user.sum(1).unsqueeze(1)
+
+        # members_stack =torch.stack([torch.mm(group_user,user_embed_asp[i])*aspect_softmax[:,i][:,None]/group_user.sum(1).unsqueeze(1) for i in range(len(self.aspects))],dim=1)
+        # learned_group_embed = members_stack.sum(1)
         updated_group_embed = (embed_centers + learned_group_embed)/2
         # Mean
         update_g_to_u = torch.mm(torch.t(group_user),updated_group_embed)/torch.t(group_user).sum(1).unsqueeze(1)
+        # pdb.set_trace()
         user_embeds = (self.user_embedding + update_g_to_u)/2
 
-        return [user_embeds]
+        return user_embeds, updated_group_embed
         
 
 
